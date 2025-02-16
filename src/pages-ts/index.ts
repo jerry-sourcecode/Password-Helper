ea4455a5ed63a94c69eecb7dedb61d379ffe42e3
jerry-sourcecode <JerryLiwh@hotmail.com>
2025-02-16 11:38:25 +0800

    feat(safety): 增加安全性提示
    在展示密码页面增加了安全性提示。

--- src/pages-ts/index.ts
+++ src/pages-ts/index.ts
@@ -1,3 +1,202 @@
+const simplePwd: Array<string> = [
+    "12345",
+    "123456",
+    "123456789",
+    "test1",
+    "password",
+    "12345678",
+    "asdf",
+    "qwerty",
+    "1234567890",
+    "1234567",
+    "1234",
+    "abc123",
+    "111111",
+    "123123",
+    "password1",
+    "987654321",
+    "qwertyuiop",
+    "mynoob",
+    "123321",
+    "666666",
+    "18atcskd2w",
+    "7777777",
+    "1q2w3e4r",
+    "654321",
+    "555555",
+    "3rjs1la7qe",
+    "google",
+    "1q2w3e4r5t",
+    "123qwe",
+    "zxcvbnm",
+    "1q2w3e",
+]
+
+const lessSimplePwd: Array<string> = [
+    "g_czechout",
+    "zinch",
+    "Aa123456.",
+    "iloveyou",
+    "dubsmash",
+    "test",
+    "princess",
+    "sunshine",
+    "BvtTest123",
+    "11111",
+    "ashley",
+    "00000",
+    "000000",
+    "monkey",
+    "livetest",
+    "55555",
+    "soccer",
+    "charlie",
+    "asdfghjkl",
+    "family",
+    "michael",
+    "123321",
+    "football",
+    "baseball",
+    "q1w2e3r4t5y6",
+    "nicole",
+    "jessica",
+    "purple",
+    "shadow",
+    "hannah",
+    "chocolate",
+    "michelle",
+    "daniel",
+    "maggie",
+    "qwerty123",
+    "hello",
+    "112233",
+    "jordan",
+    "tigger",
+    "superman",
+    "12345678910",
+    "summer",
+    "fitness",
+    "bailey",
+    "fuckyou",
+    "121212",
+    "buster",
+    "butterfly",
+    "dragon",
+    "jennifer",
+    "amanda",
+    "justin",
+    "cookie",
+    "basketball",
+    "shopping",
+    "pepper",
+    "joshua",
+    "hunter",
+    "ginger",
+    "matthew",
+    "abcd1234",
+    "taylor",
+    "samantha",
+    "whatever",
+    "andrew",
+    "1qaz2wsx3edc",
+    "thomas",
+    "jasmine",
+    "animoto",
+    "madison",
+    "0987654321",
+    "54321",
+    "flower",
+    "Password",
+    "maria",
+    "babygirl",
+    "lovely",
+    "sophie",
+    "Chegg123",
+    "a123456",
+    "a123456789",
+    "woaini1314",
+    "qq123456",
+    "abc123456",
+    "123456a",
+    "123456789a",
+    "147258369",
+    "a12345678",
+    "qq123456789",
+    "123456789.",
+    "7708801314520",
+    "woaini",
+    "5201314520",
+    "q123456",
+    "123456abc",
+    "1233211234567",
+    "123456.",
+    "0123456789",
+    "asd123456",
+    "aa123456",
+    "135792468",
+    "q123456789",
+    "abcd123456",
+    "12345678900",
+    "woaini520",
+    "woaini123",
+    "zxcvbnm123",
+    "1111111111111111",
+    "w123456",
+    "aini1314",
+    "abc123456789",
+    "woaini521",
+    "1314520520",
+    "1234567891",
+    "qwe123456",
+    "asd123",
+    "1472583690",
+    "1357924680",
+    "789456123",
+    "123456789abc",
+    "z123456",
+    "1234567899",
+    "aaa123456",
+    "www123456",
+    "123456789q",
+    "123abc",
+    "qwe123",
+    "w123456789",
+    "7894561230",
+    "123456qq",
+    "zxc123456",
+    "123456789qq",
+    "1111111111",
+    "111111111",
+    "0000000000000000",
+    "1234567891234567",
+    "qazwsxedc",
+    "123456..",
+    "zxc123",
+    "1234554321",
+    "123456q",
+    "123456aa",
+    "9876543210",
+    "110120119",
+    "qaz123456",
+    "qq5201314",
+    "123698745",
+    "5201314",
+    "as123456",
+    "5841314520",
+    "z123456789",
+    "52013145201314",
+    "a123123",
+    "caonima",
+    "a5201314",
+    "wang123456",
+    "abcd123",
+    "woaini1314520",
+    "123456asd",
+    "aa123456789",
+    "741852963",
+    "a12345678"
+  ];
+
 const showNoteMaxLength: number = 152; // 在main页面显示备注的最大长度
 const showOtherMaxLength: number = 60; // 在main页面显示来源、用户名、密码的最大长度
 const showPathMaxLength: number = 65; // 在main页面显示路径的最大长度
@@ -854,6 +1053,7 @@ function showPwd(by: Array<Password>, index: number, from : Folder) : void{
     <div class="formItem_Copy"><label for="from">来源：</label><input type="text" id="from" class="vaild" value="${by[index].from}" readonly /><img class="icon" src="./resources/copy.png" id="fromCopy" title="复制"></div>
     <div class="formItem_Copy"><label for="uname">用户名：</label><input type="text" id="uname" class="vaild" value="${by[index].uname}" readonly /><img class="icon" src="./resources/copy.png" id="unameCopy" title="复制"></div>
     <div class="formItem_Copy"><label for="pwd">密码：</label><input type="text" id="pwd" class="vaild" value="${by[index].pwd}" readonly /><img class="icon" src="./resources/copy.png" id="pwdCopy" title="复制"></div>
+    <div class="formItem" id="safety"></div>
     <div class="formItem_Copy"><label for="email">邮箱：</label><input type="text" id="email" class="vaild" value="${by[index].email}" readonly /><img class="icon" src="./resources/copy.png" id="emailCopy" title="复制"></div>
     <div class="formItem_Copy"><label for="phone">手机号：</label><input type="text" id="phone" class="vaild" value="${by[index].phone}" readonly /><img class="icon" src="./resources/copy.png" id="phoneCopy" title="复制"></div>
     <div class="formItem_Copy"><p>修改时间：${getReadableTime(by[index].moDate)}</p></div>
@@ -863,6 +1063,45 @@ function showPwd(by: Array<Password>, index: number, from : Folder) : void{
     <div class="action" id="back"><p>返回</p></div>
     `
     main!.innerHTML = inner;
+
+    const safety : HTMLDivElement = document.querySelector("#safety")!;
+    if (from != Folder.bin()) {
+        let list : Array<number> = [];
+        for(let i = 0; i < pwdList.length; i++){
+            if (pwdList[i].pwd == by[index].pwd && i != index){
+                list.push(i);
+            }
+        }
+        if (list.length == 1){
+            safety.innerHTML += `<p style="color: orange">此密码与${pwdList[list[0]].from}的密码是重复的。</p>`;
+        } else if (list.length == 2){
+            safety.innerHTML += `<p style="color: orange">此密码与${pwdList[list[0]].from}和${pwdList[list[1]].from}的密码是重复的。</p>`;
+        } else if (list.length > 2){
+            safety.innerHTML += `<p style="color: red">此密码与${pwdList[list[0]].from}、${pwdList[list[1]].from}等${list.length}个来源的密码是重复的。</p>`;
+        }
+
+        let isR : boolean = false;
+        for(let i = 0; i < simplePwd.length; i++){
+            if (by[index].pwd == simplePwd[i]){
+                safety.innerHTML += `<p style="color: red">此密码很容易暴露。</p>`;
+                isR = true;
+            }
+        }
+        if (!isR) for(let i = 0; i < lessSimplePwd.length; i++){
+            if (by[index].pwd == lessSimplePwd[i]){
+                safety.innerHTML += `<p style="color: orange">此密码比较容易暴露。</p>`;
+                isR = true;
+            }
+
+        }
+        if (!isR){
+            if (!isNaN(Number(by[index].pwd)))
+                safety.innerHTML += `<p style="color: orange">此密码只包含数字，比较容易暴露。</p>`;
+            if (by[index].pwd.length <= 3)
+                safety.innerHTML += `<p style="color: orange">此密码太短，比较容易暴露。</p>`;
+        }
+        if (safety.innerHTML == "") safety.style.display = "none";
+    }
     document.querySelector("#fromCopy")?.addEventListener("click", () => {
         if (document.querySelector("#from")?.getAttribute("copyed") == "true"){
             return;
@@ -1079,6 +1318,7 @@ function setting() : void {
 }
 
 window.fs.read("./data").then((data) => {
+    data = data.replace(/\s/g,'')
     let obj = JSON.parse(data);
     const salt = obj.salt;
 
